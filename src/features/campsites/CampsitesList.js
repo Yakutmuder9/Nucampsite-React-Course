bbd7cd3701798719a2c708eb7c9bdb40a4709b17
Yakutmuder9 <yakutmuder9@gmail.com>
2022-05-19 18:02:24 -0400

    CampsiteDetailPage with useParams

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -2,24 +2,18 @@ import { Col, Row } from "reactstrap";
 import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from "./campsitesSlice";
 
-const CampsitesList = ({ setCampsiteId }) => {
+const CampsitesList = () => {
   const campsites = selectAllCampsites();
-
   return (
-    <Row className="ms-auto">
-      {campsites.map((campsite) => {
-        return (
-          <Col
-            md="5"
-            className="m-4"
-            key={campsite.id}
-            onClick={() => setCampsiteId(campsite.id)}
-          >
-            <CampsiteCard campsite={campsite} />
-          </Col>
-        );
-      })}
-    </Row>
+      <Row className='ms-auto'>
+          {campsites.map((campsite) => {
+              return (
+                  <Col md='5' className='m-4' key={campsite.id}>
+                      <CampsiteCard campsite={campsite} />
+                  </Col>
+              );
+          })}
+      </Row>
   );
 };
 
--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,25 +1,12 @@
-import { useState } from "react";
-import { Container, Row, Col } from "reactstrap";
-import CampsiteDetail from "../features/campsites/CampsiteDetail";
-import CampsitesList from "../features/campsites/CampsitesList";
-import { selectCampsiteById } from "../features/campsites/campsitesSlice";
+import { Container } from 'reactstrap';
+import CampsitesList from '../features/campsites/CampsitesList';
 
 const CampsitesDirectoryPage = () => {
-  const [campsiteId, setCampsiteId] = useState(0);
-  const selectedCampsite = selectCampsiteById(campsiteId);
-
-  return (
-    <Container>
-      <Row>
-        <Col sm="5" md="7">
-          <CampsitesList setCampsiteId={setCampsiteId} />
-        </Col>
-        <Col sm="7" md="5">
-          <CampsiteDetail campsite={selectedCampsite} />
-        </Col>
-      </Row>
-    </Container>
-  );
+    return (
+        <Container>
+            <CampsitesList />
+        </Container>
+    );
 };
 
 export default CampsitesDirectoryPage;
