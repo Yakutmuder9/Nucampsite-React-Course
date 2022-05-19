e72c0b185d513e77c11ab5000786fa782f7b873d
Yakutmuder9 <yakutmuder9@gmail.com>
2022-05-19 16:27:38 -0400

    CampsitesDirectoryPage and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import CampsitesList from './features/campsites/CampsitesList.js';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import './App.css';
 
 function App() {
     return (
         <div className='App'>
-             <Header />
-            <CampsitesList campsite={CampsitesList[0]}/>
+            <Header />
+            <CampsitesDirectoryPage />
             <Footer />
         </div>
     );
