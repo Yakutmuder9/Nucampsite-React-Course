ba70318d7dacc88eb6d4a212eb18c402f420fbbc
Yakutmuder9 <yakutmuder9@gmail.com>
2022-05-19 17:02:40 -0400

    useState

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,21 +1,26 @@
-import { Col, Row, } from 'reactstrap';
+import { Col, Row } from "reactstrap";
 import CampsiteCard from "./CampsiteCard";
-import { selectAllCampsites } from './campsitesSlice';
+import { selectAllCampsites } from "./campsitesSlice";
 
-const CampsitesList = () => {
-    const campsites = selectAllCampsites();
+const CampsitesList = ({ setCampsiteId }) => {
+  const campsites = selectAllCampsites();
 
-    return (     
-        <Row className="ms-auto">
-            {campsites.map((campsite) => {
-                return (
-                    <Col md="5" className="m-4" key={campsite.id} >
-                        <CampsiteCard campsite={campsite} />
-                    </Col>
-                );
-            })}
-        </Row>  
-    );
+  return (
+    <Row className="ms-auto">
+      {campsites.map((campsite) => {
+        return (
+          <Col
+            md="5"
+            className="m-4"
+            key={campsite.id}
+            onClick={() => setCampsiteId(campsite.id)}
+          >
+            <CampsiteCard campsite={campsite} />
+          </Col>
+        );
+      })}
+    </Row>
+  );
 };
 
 export default CampsitesList;
--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,31 +1,25 @@
-import { Container, Row, Col, Button } from 'reactstrap';
-import CampsiteDetail from '../features/campsites/CampsiteDetail';
-import CampsitesList from '../features/campsites/CampsitesList';
-import { selectRandomCampsite } from '../features/campsites/campsitesSlice';
+import { useState } from "react";
+import { Container, Row, Col } from "reactstrap";
+import CampsiteDetail from "../features/campsites/CampsiteDetail";
+import CampsitesList from "../features/campsites/CampsitesList";
+import { selectCampsiteById } from "../features/campsites/campsitesSlice";
 
 const CampsitesDirectoryPage = () => {
-    let selectedCampsite = selectRandomCampsite();
+  const [campsiteId, setCampsiteId] = useState(0);
+  const selectedCampsite = selectCampsiteById(campsiteId);
 
-    const toggleCampsite = () => {
-        selectedCampsite = selectRandomCampsite();
-        console.log(selectedCampsite);
-    };
-
-    return (
-        <Container>
-            <Button onClick={() => toggleCampsite()}>
-                Select Random Campsite
-            </Button>
-            <Row>
-                <Col sm='5' md='7'>
-                    <CampsitesList />
-                </Col>
-                <Col sm='7' md='5'>
-                    <CampsiteDetail campsite={selectedCampsite} />
-                </Col>
-            </Row>
-        </Container>
-    );
+  return (
+    <Container>
+      <Row>
+        <Col sm="5" md="7">
+          <CampsitesList setCampsiteId={setCampsiteId} />
+        </Col>
+        <Col sm="7" md="5">
+          <CampsiteDetail campsite={selectedCampsite} />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default CampsitesDirectoryPage;
