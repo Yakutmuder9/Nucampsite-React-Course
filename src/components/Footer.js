1bfa04d9065fe2f48ab42f5c02e65cfe0468483b
Yakutmuder9 <yakutmuder9@gmail.com>
2022-05-19 17:38:14 -0400

    Header and Footer Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,30 @@
-import { Container, Row, Col} from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
-  return (
-    <footer className='site-footer'>
-        <Container>
-            <Row>
-                <Col xs={{ size: 4, offset: 1 }} sm='2'>
-                <h5>this will be site navigation!</h5>
-                </Col>
-                <Col xs='6' sm='3' className='text-center'>
-                <h5>Social</h5>
+    return (
+        <footer className='site-footer'>
+            <Container>
+                <Row>
+                    <Col xs={{ size: 4, offset: 1 }} sm='2'>
+                        <h5>Links</h5>
+                        <ul className='list-unstyled'>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/directory'>Directory</Link>
+                            </li>
+                            <li>
+                                <Link to='/about'>About</Link>
+                            </li>
+                            <li>
+                                <Link to='/contact'>Contact</Link>
+                            </li>
+                        </ul>
+                    </Col>
+                    <Col xs='6' sm='3' className='text-center'>
+                        <h5>Social</h5>
                         <a
                             className='btn btn-social-icon btn-instagram'
                             href='http://instagram.com/'
@@ -34,9 +49,9 @@ const Footer = () => {
                         >
                             <i className='fa fa-youtube' />
                         </a>
-                </Col>
-                <Col sm='4' className='text-center'> 
-                <a
+                    </Col>
+                    <Col sm='4' className='text-center'>
+                        <a
                             role='button'
                             className='btn btn-link'
                             href='tel:+12065551234'
@@ -51,11 +66,11 @@ const Footer = () => {
                         >
                             <i className='fa fa-envelope-o' /> campsites@nucamp.co
                         </a>
-                </Col>
-            </Row>
-        </Container>
-    </footer>
-  )
-}
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,51 @@
-import { Navbar, NavbarBrand } from 'reactstrap'
-import NucampLogo from '../app/assets/img/logo.png'
+import { useState } from 'react';
+import { 
+    Navbar, 
+    NavbarBrand,
+    Collapse,
+    NavbarToggler,
+    Nav, 
+    NavItem,
+} from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import NucampLogo from '../app/assets/img/logo.png';
 
 const Header = () => {
-  return (
-    <Navbar dark color='primary' sticky='top' expand='md'>
-        <NavbarBrand href='/'>
-            <img src={NucampLogo} alt='nucamp logo' />
-        </NavbarBrand>
-</Navbar>
-  )
-}
+    const [menuOpen, setMenuOpen] = useState(false);
 
-export default Header
+    return (
+        <Navbar dark color='primary' sticky='top' expand='md'>
+            <NavbarBrand className='ms-5' href='/'>
+                <img src={NucampLogo} alt='nucamp logo' className='float-start' />
+                <h1 className='mt-1'>NuCamp</h1>
+            </NavbarBrand>
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)}/>
+            <Collapse isOpen={menuOpen} navbar>
+                <Nav className='ms-auto' navbar>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/'>
+                            <i className='fa fa-home fa-lg' /> Home
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/directory'>
+                            <i className='fa fa-list fa-lg' /> Directory
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/about'>
+                            <i className='fa fa-info fa-lg' /> About
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/contact'>
+                            <i className='fa fa-address-card fa-lg' /> Contact
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
+    );
+};
+
+export default Header;
