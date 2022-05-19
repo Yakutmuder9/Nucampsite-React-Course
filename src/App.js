c3006b4b5c11875f801474e9d79cea7262eaf942
Yakutmuder9 <yakutmuder9@gmail.com>
2022-05-19 16:13:30 -0400

    campsitesSlice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import NucampLogo from './app/assets/img/logo.png';
 import './App.css';
+import CampsitesList from './features/campsites/CampsitesList.js';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
     return (
         <div className='App'>
-            <Navbar dark color='primary' sticky='top' expand='md'>
-                <Container>
-                    <NavbarBrand href='/'>
-                        <img src={NucampLogo} alt='nucamp logo' />
-                    </NavbarBrand>
-                </Container>
-            </Navbar>
-            I'm ready for workshop!
+             <Header />
+            <CampsitesList campsite={CampsitesList[0]}/>
+            <Footer />
         </div>
     );
 }
