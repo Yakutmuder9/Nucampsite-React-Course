4ac3001ca5bb3dbf1fe0c0c50c238d48aa9ee7ac
Yakutmuder9 <yakutmuder9@gmail.com>
2022-05-28 03:27:48 -0400

    addComment

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -9,9 +9,12 @@ import {
 } from "reactstrap";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { validateCommentForm } from "../../utils/validateCommentForm";
+import { useDispatch } from "react-redux";
+import { addComment } from "./commentsSlice";
 
 const CommentForm = ({ campsiteId }) => {
   const [modalOpen, setModalOpen] = useState(false);
+  const dispatch = useDispatch();
 
   const handleSubmit = (values) => {
     const comment = {
@@ -19,8 +22,11 @@ const CommentForm = ({ campsiteId }) => {
       rating: values.rating,
       author: values.author,
       text: values.commentText,
-    };
+      date: new Date(Date.now()).toISOString()
+  };
+    
     console.log(comment);
+    dispatch(addComment(comment));
     setModalOpen(false);
   };
   return (
--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -7,10 +7,23 @@ const initialState = {
 
 const commentsSlice = createSlice({
     name: 'comments',
-    initialState
+    initialState,
+    reducers: {
+        addComment: (state, action) => {
+            console.log('addComment action.payload:', action.payload);
+            console.log('addComment state.commentsArray:', state.commentsArray);
+            const newComment = {
+                id: state.commentsArray.length + 1,
+                ...action.payload
+            };
+            state.commentsArray.push(newComment);
+        }
+    }
 });
 
 export const commentsReducer = commentsSlice.reducer;
+
+export const { addComment } = commentsSlice.actions;
 
 export const selectCommentsByCampsiteId = (campsiteId) => (state) => {
     return state.comments.commentsArray.filter(
